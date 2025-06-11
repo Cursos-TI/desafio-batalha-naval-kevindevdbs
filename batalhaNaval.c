9484d5ca5ab4e54ba4da53a1ffa3ff02eb0b450b
Kevin Dias <kevindevdbs@gmail.com>
2025-06-11 22:20:49 +0000

    Desafio Aventureiro Completo.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,56 +1,97 @@
 #include <stdio.h>
 
-int main() {
-    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Char para o Cabeçalho de Colunas
+#define TAM 10
+#define NAVIO 3
+#define AGUA 0
 
-    //Matriz para o tabuleiro
+// Função para verificar se é possível posicionar um navio
+int pode_posicionar(int tabuleiro[TAM][TAM], int linha, int coluna, int direcao, int diagonal) {
+    linha--; // Ajustar para índice da matriz (0-9)
+    coluna--;
 
-    int tabuleiro[11][11] = {
-        {0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {2, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0},
-        {3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {4, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-        {5, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-        {6, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0},
-        {7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    for (int i = 0; i < 3; i++) {
+        int l = linha, c = coluna;
 
-    printf("---------------BATALHA NAVAL---------------\n\n");
+        if (diagonal == 1) {
+            l = linha + i;
+            c = coluna + i;
+        } else if (diagonal == 2) {
+            l = linha + i;
+            c = coluna - i;
+        } else if (direcao == 0) {
+            c = coluna + i;
+        } else if (direcao == 1) {
+            l = linha + i;
+        }
 
-    // Imprimir cabeçalho das colunas
+        if (l < 0 || l >= TAM || c < 0 || c >= TAM || tabuleiro[l][c] != AGUA) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Função para posicionar o navio no tabuleiro
+void posicionar_navio(int tabuleiro[TAM][TAM], int linha, int coluna, int direcao, int diagonal) {
+    linha--; coluna--; // Ajustar índice
+
+    for (int i = 0; i < 3; i++) {
+        int l = linha, c = coluna;
+
+        if (diagonal == 1) {
+            l = linha + i;
+            c = coluna + i;
+        } else if (diagonal == 2) {
+            l = linha + i;
+            c = coluna - i;
+        } else if (direcao == 0) {
+            c = coluna + i;
+        } else if (direcao == 1) {
+            l = linha + i;
+        }
+
+        tabuleiro[l][c] = NAVIO;
+    }
+}
+
+// Função para exibir o tabuleiro (de 1 a 10)
+void exibir_tabuleiro(int tabuleiro[TAM][TAM]) {
     printf("    ");
-
-    for (int j = 0; j < 10; j++) {
-        printf(" %c ", linha[j]);
-        
+    for (int i = 1; i <= TAM; i++) {
+        printf("%2d ", i);
     }
     printf("\n");
 
-    //Imprime o Tabuleiro
-
-    for (int i = 1; i < 11; i++)
-    {
-        printf(" %2d ", i );
-
-        for (int j = 1; j < 11; j++)
-        {
-           printf(" %d " , tabuleiro[i][j]);
-        };
-
+    for (int i = 0; i < TAM; i++) {
+        printf("%2d |", i + 1);
+        for (int j = 0; j < TAM; j++) {
+            printf("%2d ", tabuleiro[i][j]);
+        }
         printf("\n");
-        
     }
-    
-    printf("\n");
+}
 
-     printf("---------------BATALHA NAVAL---------------\n\n");
+int main() {
+    int tabuleiro[TAM][TAM] = {0};
 
+    // Coordenadas de 1 a 10 
+    if (pode_posicionar(tabuleiro, 1, 1, 0, 0)) { // Horizontal
+        posicionar_navio(tabuleiro, 1, 1, 0, 0);
+    }
 
-    
-    
+    if (pode_posicionar(tabuleiro, 5, 2, 1, 0)) { // Vertical
+        posicionar_navio(tabuleiro, 5, 2, 1, 0);
+    }
+
+    if (pode_posicionar(tabuleiro, 3, 3, 0, 1)) { // Diagonal principal
+        posicionar_navio(tabuleiro, 3, 3, 0, 1);
+    }
+
+    if (pode_posicionar(tabuleiro, 6, 8, 0, 2)) { // Diagonal secundária
+        posicionar_navio(tabuleiro, 6, 8, 0, 2);
+    }
+
+    exibir_tabuleiro(tabuleiro);
+
     return 0;
 }
